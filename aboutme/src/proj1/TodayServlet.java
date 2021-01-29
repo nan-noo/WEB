80efca9a8bacfb1b1725d6c8d569df982c62ad9e
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-01-29 20:51:52 +0900

    Update TodayServlet.java

--- aboutme/src/proj1/TodayServlet.java
+++ aboutme/src/proj1/TodayServlet.java
@@ -1,6 +1,10 @@
 package proj1;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,13 +22,19 @@ public class TodayServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<a href='index.html'>메인화면<a>");
+		
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter form = DateTimeFormatter.ofPattern("yyyy/M/d hh:mm");
+		String nowStr = now.format(form);
+		
+		out.println("<h1>현재시간: " + nowStr + "</h1>");
+		
 	}
 
 }
