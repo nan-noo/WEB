0833218845e1d4e85a60bba2494c35b8ea827c6d
nan-noo <54002105+nan-noo@users.noreply.github.com>
2021-03-08 23:50:52 +0900

    create colors.js

---
+++ colors.js
@@ -0,0 +1,32 @@
+var Color = {
+    "setColorAll": function(tag, color){
+        var i = 0;
+        var list = document.querySelectorAll(tag);
+
+        while(i < list.length){
+            list[i].style.color = color;
+            i = i + 1;
+        }
+    },
+    "setColor": function(tag, color){
+        document.querySelector(tag).style.color = color;
+    },
+    "setBgColor": function(tag, color){
+        document.querySelector(tag).style.backgroundColor = color;
+    }
+}
+
+function nightDayHandler(self){
+    if(self.value === 'night'){
+        Color.setBgColor('body', 'black');
+        Color.setColor('body', 'white');
+        self.value = 'day';
+        Color.setColorAll('a', 'powderblue');
+    }
+    else{
+        Color.setBgColor('body', 'white');
+        Color.setColor('body', 'black');
+        self.value = 'night';
+        Color.setColorAll('a', 'blue');
+    }
+}
